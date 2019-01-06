ae045efe0d0531cd1240fcb5a206a1e2dfe09c2b
Stephen Carragher <car07009312@hotmail.co.uk>
2019-01-06 19:40:22 +0000

    initial

---
+++ test/src/game_pb.d.ts
@@ -0,0 +1,65 @@
+// package: uss
+// file: game.proto
+
+import * as jspb from "google-protobuf";
+import * as google_protobuf_timestamp_pb from "google-protobuf/google/protobuf/timestamp_pb";
+
+export class Game extends jspb.Message {
+  getId(): string;
+  setId(value: string): void;
+
+  hasPlayer1(): boolean;
+  clearPlayer1(): void;
+  getPlayer1(): Player | undefined;
+  setPlayer1(value?: Player): void;
+
+  hasPlayer2(): boolean;
+  clearPlayer2(): void;
+  getPlayer2(): Player | undefined;
+  setPlayer2(value?: Player): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): Game.AsObject;
+  static toObject(includeInstance: boolean, msg: Game): Game.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: Game, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): Game;
+  static deserializeBinaryFromReader(message: Game, reader: jspb.BinaryReader): Game;
+}
+
+export namespace Game {
+  export type AsObject = {
+    id: string,
+    player1?: Player.AsObject,
+    player2?: Player.AsObject,
+  }
+}
+
+export class Player extends jspb.Message {
+  getName(): string;
+  setName(value: string): void;
+
+  getX(): number;
+  setX(value: number): void;
+
+  getY(): number;
+  setY(value: number): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): Player.AsObject;
+  static toObject(includeInstance: boolean, msg: Player): Player.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: Player, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): Player;
+  static deserializeBinaryFromReader(message: Player, reader: jspb.BinaryReader): Player;
+}
+
+export namespace Player {
+  export type AsObject = {
+    name: string,
+    x: number,
+    y: number,
+  }
+}
